f8bb25294e6495772180a29314f1215ae75fdfc7
plorenzom <plorenzo.manzano@gmail.com>
2026-09-02 17:06:56 +0200

    feat: add application entry point

---
+++ src/main/java/es/thefactory/hellospringdatajpa/Application.java
@@ -0,0 +1,17 @@
+package es.thefactory.hellospringdatajpa;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author Pablo Lorenzo Manzano.
+ */
+public class Application {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+    }
+}
